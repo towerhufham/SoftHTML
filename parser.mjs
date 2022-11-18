4938373241cf4046385cf116353f3afe4378318d
towerhufham <32599109+towerhufham@users.noreply.github.com>
2022-11-17 19:07:28 -0500

    formatting classes & attributes

--- parser.mjs
+++ parser.mjs
@@ -1,10 +1,29 @@
+const formatClasses = classes => {
+    //split and rejoin to ensure they look ok
+    const classesStr = classes.join(" ")
+    return `class="${classesStr}"`
+}
+
+const formatAttributes = attributes => {
+    return attributes.map(a => `${a[0]}="${a[1]}"`).join(" ")
+}
+
+const formatElement = (tag, classes, attributes, content) => {
+    return `<${tag} ${formatAttributes(attributes)} ${formatClasses(classes)}>${content}</${tag}>\n`
+}
+
 export const parseSoft = fullText => {
     const lines = fullText.split("\n")
     let result = ""
     for (const line of lines) {
-        const element = line.split(" ")[0]
+        const tag = line.split(" ")[0]
         const content = line.match(/ (.+)/)[1]
-        result += `<${element}>${content}</${element}>\n`
+        result += formatElement(
+            tag, 
+            ["bg-blue-500", "py-4"], 
+            [["id", "cool"], ["x-text", "var"]],
+            content
+        )
     }
     return result
 }
