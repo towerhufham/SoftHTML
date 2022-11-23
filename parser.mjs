3240f0a3a0accacbca329505902177be05e00329
towerhufham <32599109+towerhufham@users.noreply.github.com>
2022-11-22 20:02:23 -0500

    Rewrite: Hash block parser

--- parser.mjs
+++ parser.mjs
@@ -29,3 +29,35 @@
 //   generateHTML(areaTable): dict(string: treeNode) -> string
 //      this is basically the regular parser logic I have
 //      need a way to check for recursion eventually
+
+const parseHashBlocks = fullText => {
+    //if the first line doesn't start with a #, it's an implicit #root at the top
+    if (fullText[0] !== "#") {
+        fullText = "#root\n" + fullText
+    }
+    //break into lines, removing unused chars
+    const lines = fullText.replaceAll("\r", "").split("\n")
+    let blocks = []
+    let currentBlock = []
+    for (const line of lines) {
+        //if it starts with a #, preceeded by optional whitespace
+        if (/^\s*\#/.test(line)) {
+            //start new block
+            if (blocks.length > 0) {
+                //if this isn't our first block, push to the list
+                blocks.push([...currentBlock])
+                currentBlock = []
+            }
+        }
+        //add line to current block
+        currentBlock.push(line)
+    }
+    //finish block
+    blocks.push([...currentBlock])
+    return blocks
+}
+
+export const parseSoft = fullText => {
+    console.log(parseHashBlocks(fullText))
+    return "hi lol"
+}
